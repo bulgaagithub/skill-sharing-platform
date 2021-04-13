e570d5b787a44c532a635448a84f7b1cc9ce9132
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2021-04-13 22:11:45 +0800

    add dockerfile and some update env files conf

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -1,7 +1,6 @@
 import { getAllArticles } from "lib/api";
-const asyncHandler = require("middleware/asyncHandler")
 
-export default asyncHandler(async (req, res, next) => {
+export default async (req, res, next) => {
     const page = parseInt(req.query.page, 10);
     const limit = parseInt(req.query.limit, 10);
     const status = req.query.status;
@@ -9,4 +8,4 @@ export default asyncHandler(async (req, res, next) => {
     const articles = await getAllArticles(page, limit, status);
       
     res.status(200).json(articles);
-});
+};
